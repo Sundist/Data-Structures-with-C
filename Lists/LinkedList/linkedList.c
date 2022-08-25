ae877ee1d474eebf5a93bad7f8db2d06593e2169
Sundist <sundisttt@gmail.com>
2022-08-25 13:50:41 +0300

    -Lists dir -Sorts dir

---
+++ Lists/LinkedList/linkedList.c
@@ -0,0 +1,19 @@
+//
+// Created by Sundist on 25.08.2022.
+//
+
+#include <stdio.h>
+
+struct Node {
+    int data;
+    struct node *next;
+};
+
+typedef struct Node node;
+
+int size()
+
+
+int main() {
+    printf("Hello world");
+}
