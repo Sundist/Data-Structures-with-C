ea642f0131451970e848e906dbc9816581bbbea5
Sundist <sundisttt@gmail.com>
2022-08-30 16:58:46 +0300

    -ArrayList Completed ...

--- Sorts/QuickSort/cmake-build-debug/CMakeFiles/Data_Structures_with_C.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for Data_Structures_with_C.
-# This may be replaced when dependencies are built.
--- Sorts/QuickSort/quickSort.c
+++ Sorts/QuickSort/quickSort.c
@@ -3,6 +3,8 @@
 //
 #include <stdio.h>
 
+int SIZE = 15;
+
 void sort(int arr[]);
 
 int partition(int partition[], int low, int high);
@@ -10,11 +12,18 @@ int partition(int partition[], int low, int high);
 void quickSort(int partition[], int low, int high);
 
 int main() {
-    printf("Quick Sort implementation with C");
+    printf("Quick Sort implementation with C\n");
+    printf("Unsorted Array: \n");
+    int arr[SIZE];
+    for (int i = 0; i < SIZE; ++i)
+        printf("%d, ", arr[i]);
+    sort(arr);
+
 }
 
 
 void sort(int arr[]) {
+    printf("\nSorted Array:\n");
     quickSort(arr, 0, (sizeof(arr) / sizeof(int)) - 1);
 }
 
