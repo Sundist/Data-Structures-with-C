8825703b00ae061d044aab9ede26ac58ccd18f72
Sundist <sundisttt@gmail.com>
2022-09-02 22:25:05 +0300

    BSTree modified but not finished...

--- Trees/BSTree/BSTree.c
+++ Trees/BSTree/BSTree.c
@@ -13,39 +13,74 @@ struct Node {
 
 struct Node *add(struct Node *root, int data) {
     if (root == NULL) {
-        root = (struct Node *) malloc(sizeof(struct Node *));
+        root = (struct Node *) malloc(sizeof(struct Node));
+        root->data = data;
         root->left = NULL;
         root->right = NULL;
-        root->data = data;
+        return root;
     }
+
     if (data < root->data)
-        root = add(root->left, data);
+        root->left = add(root->left, data);
     else if (data > root->data)
-        root= add(root->right, data);
+        root->right = add(root->right, data);
     return root;
+}
+
+void inOrderTraverse(struct Node *root) {
+    if (root != NULL) {
+        inOrderTraverse(root->left);
+        printf("%d ", root->data);
+        inOrderTraverse(root->right);
+    }
+}
+
+void preOrderTraverse(struct Node *root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preOrderTraverse(root->left);
+        preOrderTraverse(root->right);
+    }
+}
+
+void postOrderTraverse(struct Node *root) {
+    if (root != NULL) {
+        postOrderTraverse(root->left);
+        postOrderTraverse(root->right);
+        printf("%d ", root->data);
+    }
 }
 
 int search(struct Node *root, int data) {
     if (root == NULL)
-        return -1;
-    if (data > root->data)
-        search(root->right, data);
-    else if (data < root->data)
-        search(root->left, data);
-    else return 1;
-    return 0;
-
+        return 0;
+    else if (root->data == data)
+        return 1;
+    else {
+        if (data < root->data)
+            search(root->left, data);
+        else if (data > root->data) {
+            search(root->right, data);
+        }
+    }
+    return -1;
 }
 
-
 int main() {
-    struct Node *root = (struct Node *) malloc(sizeof(struct Node *));
-    root->data = 42;
-    root->right = NULL;
-    root->left = NULL;
-    root = add(root, 86);
-    root = add(root, 6);
-    root = add(root, 48);
-    root = add(root, 18);
-    printf("%d \n", search(root, 6));
+    struct Node *root = NULL;
+    root = add(root, 65);
+    add(root, 86);
+    add(root, 6);
+    add(root, 48);
+    add(root, 18);
+    add(root, 94);
+    add(root, 4);
+    add(root, 15);
+    preOrderTraverse(root);
+    printf("\n");
+    inOrderTraverse(root);
+    printf("\n");
+    postOrderTraverse(root);
+    printf("\n");
+    printf("%d", search(root, 66));
 }
