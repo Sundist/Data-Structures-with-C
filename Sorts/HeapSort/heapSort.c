f08e32551198b4b40556a7e86c40d81977bd8b1e
sundisttt@gmail.com <sundisttt@gmail.com>
2022-09-30 20:17:40 +0300

    Heap Sort added, not finished.

--- Sorts/HeapSort/heapSort.c
+++ Sorts/HeapSort/heapSort.c
@@ -1,3 +1,41 @@
 //
 // Created by Sundist on 30.09.2022.
 //
+
+#include <stdio.h>
+
+
+int SIZE = 0;
+int CAPACITY = 15;
+
+void add(int array, int element);
+
+int remove();
+
+int main() {
+    int list[CAPACITY];
+
+}
+
+void add(int array, int element) {
+    array[SIZE++] = element;
+    while (1){
+
+    }
+
+}
+
+int remove() {
+    // son eleman ılk elemana atanacak
+    // son eleman arryden cıkartılacak
+    // dongu ıcınde ıkı cocuk bulunup  bırınden daha kucukse buyuk olanla yer degıstırılecek
+    // left yok right yok
+    // break
+    // left var right yok
+    // //left küçük -> break
+    // //left büyük -> swap
+    //ikisi de var
+    // //buyuk olanı bul
+    // // //buyuk olan benden kucuk -> break
+    // // // buyuk olan benden buyuk -> swap
+}
